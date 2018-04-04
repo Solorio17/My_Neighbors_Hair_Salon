919b9032cca51adc12689b2677d1cf0cef9af4f6
Jason <jasonsoloriom@gmail.com>
2018-04-04 10:12:42 -0600

    Creating an array of objects that holds the names of each stylist and for testing purposes their guessed age

---
+++ src/pages/stylists/stylists.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {NavController,} from 'ionic-angular';
+
+@Component({
+  selector: 'page-stylists',
+  templateUrl: 'stylists.html',
+})
+export class StylistsPage {
+  stylists = [
+    {
+      name: "Liz",
+      age: 45
+    },
+    {
+      name: "Ambour",
+      age: 32
+    },
+    {
+      name: "Savannah",
+      age: 35
+    },
+    {
+      name: "Sasha",
+      age: 31
+    },
+    {
+      name: "Azureya",
+      age: 21
+    },
+    {
+      name: "Joy",
+      age: 40
+    },
+  ]
+
+  constructor(public navCtrl: NavController) {
+  }
+
+
+
+}
